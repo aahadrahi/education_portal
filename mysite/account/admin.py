2de0583309df04f33d1bf84aa9ad31ff932a61db
Mohd Aahad <93692744+aahadrahi@users.noreply.github.com>
2022-11-06 10:46:34 +0530

    admin file

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
-
+from .models import IntroMadrasa,Course,Targets,Openions,Teachers,Gallery,SocialAccounts
 # Register your models here.
+
+
+
+class IntroMadrasaAdmin(admin.ModelAdmin):
+    list_display=( 'logo','name', 'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+class CourseAdmin(admin.ModelAdmin):
+    list_display=( 'name','content', 'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+class TargetsAdmin(admin.ModelAdmin):
+    list_display=( 'name','content', 'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+class OpenionsAdmin(admin.ModelAdmin):
+    list_display=( 'profile_pic','name','email','title', 'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+class TeachersAdmin(admin.ModelAdmin):
+    list_display=( 'profile_pic','name','phone_no','position' ,'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+class SocialAccountsAdmin(admin.ModelAdmin):
+    list_display=( 'teacher','name',)
+    list_filter = ( 'name',)
+    search_fields = ('name', )
+    
+class GalleryAdmin(admin.ModelAdmin):
+    list_display=( 'image','content', 'created_date')
+    list_filter = ( 'created_date',)
+    search_fields = ('name', )
+    
+
+
+
+admin.site.register(IntroMadrasa,IntroMadrasaAdmin)
+admin.site.register(Course,CourseAdmin)
+admin.site.register(Targets,TargetsAdmin)
+admin.site.register(Openions,OpenionsAdmin)
+admin.site.register(Teachers,TeachersAdmin)
+admin.site.register(SocialAccounts,SocialAccountsAdmin)
+admin.site.register(Gallery,GalleryAdmin)
--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,3 +1,98 @@
 from django.db import models
-
 # Create your models here.
+
+
+# This is intro table of madarsa
+class IntroMadrasa(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    full_image = models.ImageField(upload_to='app/Post/')
+    logo = models.ImageField(upload_to='app/Post/')
+    color_one = models.CharField(max_length=200)
+    color_two = models.CharField(max_length=200)
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+# This is course table 
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    logo = models.ImageField(upload_to='app/Post/')
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+    
+    def __str__(self):
+        return self.name
+   
+# This is Mission table 
+class Targets(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='app/Post/')
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+    
+    def __str__(self):
+        return self.name
+
+        
+# This is say tabel   
+class Openions(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    profile_pic = models.ImageField(upload_to='app/Post/')
+    title = models.CharField(max_length=200)
+    message = models.TextField(max_length=1000)
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+    
+    def __str__(self):
+        return self.name
+   
+
+# This is Profile Of Mudarriseen tabel   
+class Teachers(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    profile_pic = models.ImageField(upload_to='app/Post/')
+    phone_no = models.IntegerField()
+    position = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+    
+    def __str__(self):
+        return self.name
+class SocialAccounts(models.Model):
+    teacher = models.ForeignKey(Teachers,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    link = models.CharField(max_length=800)
+    
+    def __str__(self):
+        return self.name
+# This is Gallery tabel      
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='app/Post/')
+    content = models.TextField(max_length=1000)
+    created_date = models.DateField(auto_now_add=True,null=True)
+    updated_date = models.DateField(auto_now=True)
+    Created_by = models.CharField(max_length=200,blank=True)
+    updated_by = models.CharField(max_length=200,blank=True)
+    
+    def __str__(self):
+        return self.content
+   
